2df8996af155dc7e60c76b0ad8a145fd88a6785f
inielsa <90020185+inielsa@users.noreply.github.com>
2026-01-16 19:45:34 +0700

    added more features

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@ function welcomeMessage(){
 
     document.getElementById("welcome-speech").innerText = `Welcome, ${userResponse}!`;
 }
+
+const userName = document.getElementById("username");
+const bdate = document.getElementById("bdate");
+const male = document.getElementById("male");
+const female = document.getElementById("female");
+const userMessage = document.getElementById("userMessage");
+const displayMessage = document.getElementById("displayMessage");
+let uName;
+let gender;
+let birthdate;
+let uMessage;
+
+function submitMessage(){
+    uName = userName.value;
+    birthdate = bdate.value;
+    uMessage = userMessage.value;
+    if(male.checked){
+        gender = "Male";
+    }
+    else if(female.checked){
+        gender = "Female";
+    }
+
+    // console.log(uName);
+    // console.log(gender);
+    // console.log(birthdate);
+    // console.log(uMessage);
+    displayMessage.textContent = `Message from ${uName} (${gender}, born on ${birthdate}): ${uMessage}`;
+
+}
